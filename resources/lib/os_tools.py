6883aeebff27470b88a73d5ff85bd42964522070
Ian Leonard <antonlacon@gmail.com>
2022-11-21 04:39:27 +0000

    os_tools: add function to run system commands
    This functions runs the given command, waits for it to finish, then
    determines what to return to the caller. It is either the command's output,
    or None. If the command generates an error, then it also returns None to
    signal to the caller it will have no output to work with and take appropriate
    action.
    
    With check=True, if the command's return code is not 0, a
    subprocess.CalledProcessError exception will be raised. Error information is
    sent to the log for troubleshooting. Additional information is available
    in debug if the function is suspected to be the source of the error.
    
    FileNotFoundError exception is when the command isn't found to execute.
    
    Reorganize and add some documentation to os_tools while there.
    
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- resources/lib/os_tools.py
+++ resources/lib/os_tools.py
@@ -1,9 +1,21 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
+'''This module holds support functions for interacting with the underlying OS.
+
+Support functions are grouped by purpose:
+1. File access: read / write / copy / move / download
+2. System access: executing system commands
+'''
+
 import os
+import shlex
+import subprocess
+
+import log
 
 
+### FILE ACCESS ###
 def read_shell_setting(file, default):
     setting = default
     if os.path.isfile(file):
@@ -22,3 +34,36 @@ def read_shell_settings(file, defaults={}):
                     value = value[1:-1]
                 settings[name] = value
     return settings
+
+
+### SYSTEM ACCESS ###
+def execute(command, read_input=None, get_result=False, include_stderr=True):
+    '''Run command, waiting for it to finish. Returns: command output or None'''
+    log.log(f'Executing command: {command}', log.DEBUG)
+    try:
+        # passes stdin as read_input. read_input must be bytes or None
+        if read_input:
+            if os.path.exists(read_input):
+                log.log(f'os_tools.execute: Found file as stdin: {read_input}', log.DEBUG)
+                with open(read_input, 'rb') as f:
+                    data = f.read()
+            else:
+                log.log(f'os_tools.execute: Found string as stdin: {read_input}', log.DEBUG)
+                data = read_input.encode()
+        else:
+            data = None
+        stderr_stream = subprocess.STDOUT if include_stderr else subprocess.DEVNULL
+        cmd_status = subprocess.run(shlex.split(command), input=data, check=True, stdout=subprocess.PIPE, stderr=stderr_stream)
+    except subprocess.CalledProcessError as e:
+        log.log(f'Command failed: {command}', log.ERROR)
+        log.log(f'shlex.split() output: {shlex.split(command)}', log.DEBUG)
+        log.log(f'Executed command: {e.cmd}', log.DEBUG)
+        log.log(f'exit code: {e.returncode}', log.ERROR)
+        log.log(f'START COMMAND OUTPUT:\n{e.stdout.decode()}\nEND COMMAND OUTPUT', log.ERROR)
+        # return None on failure. Commands that want output get nothing; remainder weren't expecting output
+        return None
+    except FileNotFoundError:
+        log.log(f'os_tools.execute: Command not found: {command.split(" ")[0]}', log.ERROR)
+        return None
+    # return output if requested, otherwise None
+    return cmd_status.stdout.decode() if get_result else None
