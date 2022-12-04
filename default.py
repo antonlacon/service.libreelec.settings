2a312e0d5a7064563d306922557f30a1b188b56d
Ian Leonard <antonlacon@gmail.com>
2022-12-04 07:50:41 +0000

    oe.py: move necessary directory creation to default.py
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- default.py
+++ default.py
@@ -3,10 +3,14 @@
 # Copyright (C) 2013 Lutz Fiebach (lufie@openelec.tv)
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
+import os
 import socket
 
 import xbmc
 import xbmcaddon
+
+import syspath
+from defaults import CONFIG_CACHE, XBMC_USER_HOME
 
 
 __scriptid__ = 'service.libreelec.settings'
@@ -15,6 +19,7 @@ __cwd__ = __addon__.getAddonInfo('path')
 __media__ = f'{__cwd__}/resources/skins/Default/media'
 _ = __addon__.getLocalizedString
 
+
 try:
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect('/var/run/service.libreelec.settings.sock')
@@ -22,3 +27,12 @@ try:
     sock.close()
 except Exception as e:
     xbmc.executebuiltin(f'Notification("LibreELEC", "{_(32390)}", 5000, "{__media__}/icon.png"')
+
+# set up addon settings location
+try:
+    if not os.path.exists(f'{XBMC_USER_HOME}/userdata/addon_data/service.libreelec.settings'):
+        os.makedirs(f'{XBMC_USER_HOME}/userdata/addon_data/service.libreelec.settings')
+    if not os.path.exists(f'{CONFIG_CACHE}/services'):
+        os.makedirs(f'{CONFIG_CACHE}/services')
+except Exception:
+    pass
