33c738efd7299e3660fc09b08a48873f12192512
Ian Leonard <antonlacon@gmail.com>
2022-12-04 08:26:17 +0000

    oe.py: move TEMP to defaults.py, rename to LOCAL_UPDATE_TEMP
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- resources/lib/defaults.py
+++ resources/lib/defaults.py
@@ -105,6 +105,7 @@ updates = {
     'UPDATE_REQUEST_URL': 'https://update.libreelec.tv/updates.php',
     'UPDATE_DOWNLOAD_URL': 'http://%s.libreelec.tv/%s',
     'LOCAL_UPDATE_DIR': '/storage/.update/',
+    'LOCAL_UPDATE_TEMP': f'{XBMC_USER_HOME}/temp',
     }
 
 about = {'ENABLED': True}
