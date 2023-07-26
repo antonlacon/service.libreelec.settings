257f4668880a2cfe0c9931ab265b2f5db452d44a
Ian Leonard <antonlacon@gmail.com>
2023-07-26 18:26:36 -0400

    updates.py: test for filepath when autocompleting json url
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- resources/lib/modules/updates.py
+++ resources/lib/modules/updates.py
@@ -443,7 +443,7 @@ class updates(modules.Module):
         if not url:
             url = self.UPDATE_DOWNLOAD_URL % ('releases', 'releases.json')
         if not url.startswith(('http://', 'https://', 'file://')):
-            url = f'https://{url}'
+            url = f'file://{url}' if os.path.isfile(url) else f'https://{url}'
         if not url.endswith('.json'):
             url = f'{url}/releases.json'
         data = oe.load_url(url)
