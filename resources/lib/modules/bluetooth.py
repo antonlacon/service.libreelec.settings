511fbc1759aa7e2d204bcd9b2b61e6968096abae
Ian Leonard <antonlacon@gmail.com>
2025-05-20 05:01:39 -0400

    misc: replace oe.notify with ui_tools.notification
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- resources/lib/modules/bluetooth.py
+++ resources/lib/modules/bluetooth.py
@@ -18,6 +18,8 @@ import log
 import modules
 import oe
 import oeWindows
+import ui_tools
+
 
 BT_DEVICES_LIST_REFRESH_INTERVAL_SECONDS = 5
 
@@ -258,7 +260,8 @@ class bluetooth(modules.Module):
     @log.log_function()
     def dbus_error_handler(self, error):
         log.log(f'error message: {repr(error.message)}', log.DEBUG)
-        oe.notify('Bluetooth error', error.message.split('.')[0], 'bt')
+        # FIXME localization
+        ui_tools.notification(error.message.split('.')[0], 'Bluetooth error', 'bt')
         if hasattr(self, 'pinkey_window'):
             self.close_pinkey_window()
 
